5e928acde91c07a5c2e81f10afb4a5ecadc6dc56
cheever-aiinfluencer <cheevernate@gmail.com>
2026-07-26 01:02:04 -0700

    Add src/components/Nav.jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+export default function Nav() {
+  return (
+    <header className="nav">
+      <a href="#top" className="nav__mark">
+        Nate<span className="nav__dot">.</span>
+      </a>
+      <a href="#contact" className="nav__link">
+        Get in touch
+      </a>
+    </header>
+  )
+}
